9c726ecff03a3df7b25a85f221dea0c7cbe5082e
shahedbassam <shahedbassam69@gmail.com>
2023-04-04 04:44:31 +0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,52 @@
+<?php
+
+use GuzzleHttp\Psr7\Request;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    $tasks = DB::table('tasks') -> get();
+    return view('tasks', compact('tasks'));
+
+
+});
+Route::POST('store', function ( ) {
+    $tasks = DB::table('tasks') -> insert([
+        'name' => $_REQUEST['name']
+    ]);
+    return redirect()-> back();
+
+
+});
+Route::POST('delete/{id}', function ($id) {
+    $tasks = DB::table('tasks') -> where('id','=',$id)-> delete();
+    return redirect()-> back();
+
+
+});
+Route::get('update/{id}/edit', function ($id) {
+    $tasks = DB::table('tasks') -> where('id','=',$id)
+     -> update(['options->enabled' => true]);
+
+    return redirect()-> back();
+
+
+});
+Route::pott('update/{id}/', function ($id) {
+    $tasks = DB::table('tasks') -> where('id','=',$id)
+     -> update(['options->enabled' => true]);
+
+    return redirect()-> back();
+
+
+});
